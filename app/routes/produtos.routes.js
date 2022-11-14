547aec9fe3fa59aa7be038a916b1413dc4895f2e
Josué Camelo <cloudone@josuecamelo.com>
2022-11-14 19:31:35 -0300

    Melhorias

--- app/routes/produtos.routes.js
+++ app/routes/produtos.routes.js
@@ -1,33 +1,23 @@
-/*module.exports = app => {
-  const produto = require("../controllers/produto.controller.js");
-
-  var router = require("express").Router();
-
-  // Create a new Tutorial
-  router.post("/", produto.create);
-
-  // Retrieve all Tutorials
-  router.get("/", produto.findAll);
-
-  // Retrieve a single Tutorial with id
-  router.get("/:id", produto.findOne);
-
-  // Update a Tutorial with id
-  router.put("/:id", produto.update);
-
-  // Delete a Tutorial with id
-  router.delete("/:id", produto.delete);
-
-  // Delete all Tutorials
-  router.delete("/", produto.deleteAll);
-
-  //app.use('/api/produtos', router);
-};*/
-
 var express = require('express');
 var router = express.Router();
 const produtos = require("../controllers/produto.controller.js");
  
-router.get("/", produtos.findAll);
+ // Create a new Produto
+ router.post("/", produtos.create);
+
+ // Retrieve all Produtos
+ router.get("/", produtos.findAll);
+
+ // Retrieve a single Produto with id
+ router.get("/:id", produtos.findOne);
+
+ // Update a Produto with id
+ router.put("/:id", produtos.update);
+
+ // Delete a Produto with id
+ router.delete("/:id", produtos.delete);
+
+ // Delete all Produtos
+ router.delete("/", produtos.deleteAll);
  
 module.exports = router;
--- app/routes/restaurantes.routes.js
+++ app/routes/restaurantes.routes.js
@@ -1,35 +1,24 @@
-/*module.exports = app => {
-  const restaurantes = require("../controllers/restaurante.controller.js");
-
-  var router2 = require("express").Router();
-
-  // Create a new Restaurante
-  router.post("/", restaurantes.create);
-
-  // Retrieve all Restaurantes
-  router.get("/", restaurantes.findAll);
-
-  // Retrieve a single Restaurante with id
-  router.get("/:id", restaurantes.findOne);
-
-  // Update a Restaurante with id
-  router.put("/:id", restaurantes.update);
-
-  // Delete a Restaurante with id
-  router.delete("/:id", restaurantes.delete);
-
-  // Delete all Restaurante
-  router.delete("/", restaurantes.deleteAll);
-
-  app.use('/api/restaurantes', router2);
-};*/
-
 var express = require('express');
 var router = express.Router();
 
 const restaurantes = require("../controllers/restaurante.controller.js");
  
+// Create a new Restaurante
+router.post("/", restaurantes.create);
+
 // Retrieve all Restaurantes
 router.get("/", restaurantes.findAll);
+
+// Retrieve a single Restaurante with id
+router.get("/:id", restaurantes.findOne);
+
+// Update a Restaurante with id
+router.put("/:id", restaurantes.update);
+
+// Delete a Restaurante with id
+router.delete("/:id", restaurantes.delete);
+
+// Delete all Restaurante
+router.delete("/", restaurantes.deleteAll);
 
 module.exports = router;
